80576213ac39cdc1dada0064bac06502daf5443b
Muhmmad Majid Saleem <majid.saleem@swaam.com>
2021-12-13 17:47:08 +0500

    Initial Commit

---
+++ new sample appUITests/new_sample_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  new_sample_appUITestsLaunchTests.swift
+//  new sample appUITests
+//
+//  Created by Majid on 13/12/2021.
+//
+
+import XCTest
+
+class new_sample_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
